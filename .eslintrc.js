7682bfb06ca75c69f5b098c5d83e8a956d5650a7
kosal73 <kosalarrupe@gmail.com>
2021-01-05 11:44:03 +0700

    FIX CR

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
         // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
         // e.g. "@typescript-eslint/explicit-function-return-type": "off",
         'react/prop-types': 0, // Disable React prop type
-        'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
     settings: {
         react: {
